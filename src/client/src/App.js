84ec4095c621daa22a2df4794c4952676263865b
PetarPaunov <petarp.2000@abv.bg>
2023-02-24 16:19:01 +0200

    Changes in Navbar

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,13 +1,33 @@
-import {Navbar} from './components/Navbar/Navbar'
+import {Navbar} from './components/Navbar/Navbar';
 import { Main } from './components/Main/Main';
+import { Register } from './components/Register/Register';
+import { Login } from './components/Login/Login';
+import { useState } from 'react';
 
-import './App.css'
+import {navEnum} from './components/constants/navigationConstants.js';
 
+import './App.css';
 
 function App() {
+
+  const [navButton, setNavButton] = useState(false);
+  const [userAction, setUserAction] = useState(null);
+
+  const navClickHandler = (isCliced, buttonType) => {
+    setNavButton(isCliced);
+    setUserAction(buttonType);
+  };
+
+  const closePopupHandler = () => {
+    setNavButton(false);
+  };
+
   return (
     <div className="App">
-      <Navbar />
+      {navButton && userAction == navEnum.Register && <Register closePopup={closePopupHandler} />}
+      {navButton && userAction == navEnum.Login && <Login closePopup={closePopupHandler} />}
+      
+      <Navbar clickHandler={navClickHandler}/>
       <Main />
     </div>
   );
--- src/client/src/components/Navbar/Navbar.js
+++ src/client/src/components/Navbar/Navbar.js
@@ -1,39 +1,43 @@
+import {navEnum} from '../constants/navigationConstants.js'
+
 import "./Navbar.css";
 
-export const Navbar = () => {
+export const Navbar = ({
+  clickHandler
+}) => {
   return (
     <header className="header">
-  <a href="" className="logo">
-    MB
-  </a>
-  <nav className="nav">
-    <ul className="nav-list">
-      <li className="list-item">
-        <a href="" className="link">
-          Register
-        </a>
-      </li>
-      <li className="list-item">
-        <a href="" className="link">
-          Login
-        </a>
-      </li>
-      <li className="list-item">
-        <a href="" className="link">
-          Logout
-        </a>
-      </li>
-      <li className="list-item">
-        <a href="" className="prifile-link">
-          <img
-            src="./images/avatar-659651__340.webp"
-            alt=""
-            className="profile-img"
-          />
-        </a>
-      </li>
-    </ul>
-  </nav>
-</header>
+      <a href="" className="logo">
+        MB
+      </a>
+      <nav className="nav">
+        <ul className="nav-list">
+          <li className="list-item">
+            <button onClick={() =>clickHandler(true, navEnum.Register)} className="link">
+              Register
+            </button>
+          </li>
+          <li className="list-item">
+            <button onClick={() =>clickHandler(true, navEnum.Login)} className="link">
+              Login
+            </button>
+          </li>
+          <li className="list-item">
+            <button className="link">
+              Logout
+            </button>
+          </li>
+          <li className="list-item">
+            <a href="" className="prifile-link">
+              <img
+                src="./images/avatar-659651__340.webp"
+                alt=""
+                className="profile-img"
+              />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
