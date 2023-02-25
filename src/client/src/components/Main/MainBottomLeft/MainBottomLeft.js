54ba9f5f023dbd55950a0056e2d5733b328913e0
PetarPaunov <petarp.2000@abv.bg>
2023-02-25 15:04:44 +0200

    Added loading spinner

--- src/client/src/components/Main/MainBottomLeft/MainBottomLeft.js
+++ src/client/src/components/Main/MainBottomLeft/MainBottomLeft.js
@@ -1,23 +1,31 @@
 import { useEffect, useState } from "react";
 import { PostArticle } from "../PostArticle/PostArticle";
+import { GridLoader } from "react-spinners";
+
 import "./MainBottomLeft.css";
 
 export const MainBottomLeft = () => {
 
+    const [loading, setLoading] = useState(false);
     const [posts, setPosts] = useState([]);
 
   useEffect(() => {
+    setLoading(true);
+
     fetch('http://localhost:5236/api/Post/all')
         .then(res => res.json())
         .then(result => {
             setPosts(result);
+            setLoading(false);
         })
   }, []);
 
-
   return (
-    <section className="left-part">
-      {posts.map(x => <PostArticle key={x.id} {...x}/>)}
-    </section>
+    
+      loading ? <GridLoader color="#1877f2" /> :
+
+      <section className="left-part">
+        {posts.map(x => <PostArticle key={x.id} {...x}/>)}
+      </section>
   );
 };
