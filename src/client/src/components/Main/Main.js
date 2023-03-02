114ce581b61a3043cce5b895acb7c3f72163a392
PetarPaunov <petarp.2000@abv.bg>
2023-03-02 17:36:32 +0200

    Added condition for rerendering when new post is added

--- src/client/src/components/Main/Main.js
+++ src/client/src/components/Main/Main.js
@@ -1,12 +1,36 @@
-import './Main.css';
+import "./Main.css";
 
 import { Post } from "../Post/Post";
-import { MainBottomLeft } from "./MainBottomLeft/MainBottomLeft";
+import { PostArticle } from "./PostArticle/PostArticle";
+import { GridLoader } from "react-spinners";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const spinnerStyle = {
+  position: "fixed",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+};
 
 export const Main = () => {
   const [postButton, setPostButton] = useState(false);
+  const [addedNewArticle, setAddedNewArticle] = useState(false);
+
+  const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch("http://localhost:5236/api/Post/all")
+      .then((res) => res.json())
+      .then((result) => {
+        setPosts(result);
+        setLoading(false);
+        setAddedNewArticle(false);
+      });
+  }, [addedNewArticle]);
 
   const onButtonClick = (isClicked) => {
     setPostButton(isClicked);
@@ -16,9 +40,13 @@ export const Main = () => {
     setPostButton(false);
   };
 
+  const onNewArticleAdded = (isAdded) => {
+    setAddedNewArticle(isAdded)
+  };
+
   return (
     <main className="main">
-      {postButton ? <Post closePopup={closePopupHandler} /> : null}
+      {postButton ? <Post onAddedArticle={onNewArticleAdded} closePopup={closePopupHandler} /> : null}
 
       <div className="top-part">
         <button onClick={() => onButtonClick(true)} className="add-post">
@@ -27,7 +55,17 @@ export const Main = () => {
       </div>
 
       <div className="bottom-part">
-        <MainBottomLeft />
+
+        {loading ? (
+          <GridLoader style={spinnerStyle} color="#1877f2" />
+        ) : (
+          <section className="left-part">
+            {posts.map((x) => (
+              <PostArticle key={x.id} {...x} />
+            ))}
+          </section>
+        )}
+
       </div>
     </main>
   );
--- src/client/src/components/Post/Post.js
+++ src/client/src/components/Post/Post.js
@@ -4,58 +4,62 @@ import axios from "axios";
 import "./Post.css";
 
 const initialFieldValues = {
-  title: '',
-  description: '',
+  title: "",
+  description: "",
   image: null,
 };
 
-export const Post = ({ closePopup }) => {
+export const Post = ({ closePopup, onAddedArticle }) => {
 
   const [values, setValues] = useState(initialFieldValues);
 
   const handleInputChange = (e) => {
-    const {name, value} = e.target;
+    const { name, value } = e.target;
 
-    setValues(values => ({
+    setValues((values) => ({
       ...values,
       [name]: value,
     }));
-  }
+  };
 
   const imageFileHandler = (e) => {
-      if(e.target.files && e.target.files[0]){
-        let imageFile = e.target.files[0];
+    if (e.target.files && e.target.files[0]) {
+      let imageFile = e.target.files[0];
 
-          setValues(values => ({
-            ...values,
-            image: imageFile
-          }));
-      }
-  }
+      setValues((values) => ({
+        ...values,
+        image: imageFile,
+      }));
+    }
+  };
 
   const postSubmitHandler = (e) => {
     e.preventDefault();
 
     const data = new FormData();
-    data.append('Title', values.title);
-    data.append('Description', values.description);
-    data.append('Image', values.image);
+    data.append("Title", values.title);
+    data.append("Description", values.description);
+    data.append("Image", values.image);
 
-    axios.post('http://localhost:5236/api/Post/add-post', data, {
-      headers:{
-        'Content-Type': 'multipart/form-data',
-        'Authorization' : "Bearer " + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6IjA3NDllZWQyLTAxZWUtNDQ0YS1hMjdkLThiMDA4NjJjYTkwNyIsIm5hbWVpZCI6IjA3NDllZWQyLTAxZWUtNDQ0YS1hMjdkLThiMDA4NjJjYTkwNyIsInN1YiI6IlRlc3QyQFRlc3QuY29tIiwiZW1haWwiOiJUZXN0MkBUZXN0LmNvbSIsImp0aSI6ImE2ZmNiMWM2LWU3MGYtNDMwNy1hMzg3LWM4ZjFhZWJlNjdkYSIsIm5iZiI6MTY3NzY5ODQwNywiZXhwIjoxNjc3Nzg0ODA2LCJpYXQiOjE2Nzc2OTg0MDd9.RygWp3qEhTNrswZRj7eNQrnx-O0ouxVLCRMo6Ct8lZE"
-      }
-    })
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    axios
+      .post("http://localhost:5236/api/Post/add-post", data, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization:
+            "Bearer " +
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6IjA3NDllZWQyLTAxZWUtNDQ0YS1hMjdkLThiMDA4NjJjYTkwNyIsIm5hbWVpZCI6IjA3NDllZWQyLTAxZWUtNDQ0YS1hMjdkLThiMDA4NjJjYTkwNyIsInN1YiI6IlRlc3QyQFRlc3QuY29tIiwiZW1haWwiOiJUZXN0MkBUZXN0LmNvbSIsImp0aSI6IjViMTA2ZTU3LTY4NDYtNDNmNS1iNzY4LTkwMDRjM2M3MDk0YyIsIm5iZiI6MTY3Nzc3MDk3NSwiZXhwIjoxNjc3ODU3MzczLCJpYXQiOjE2Nzc3NzA5NzV9.Bw1mnna9of9s3hcUQaRrkwbO-O84R7lt3XAOOKbKa_I",
+        },
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 
     closePopup();
-  }
+    onAddedArticle(true);
+  };
 
   return (
     <div className="container-post">
@@ -68,7 +72,14 @@ export const Post = ({ closePopup }) => {
         </div>
         <hr />
         <div className="input">
-          <input type="text" name="title" id="" placeholder="Title" value={values.title} onChange={handleInputChange} />
+          <input
+            type="text"
+            name="title"
+            id=""
+            placeholder="Title"
+            value={values.title}
+            onChange={handleInputChange}
+          />
         </div>
         <div className="input">
           <textarea
@@ -86,7 +97,12 @@ export const Post = ({ closePopup }) => {
           <label htmlFor="images" className="drop-container">
             <span className="drop-title">Drop files here</span>
             or
-            <input type="file" id="images" accept="image/*" onChange={imageFileHandler}/>
+            <input
+              type="file"
+              id="images"
+              accept="image/*"
+              onChange={imageFileHandler}
+            />
           </label>
         </div>
         <button type="submit" className="pos">
