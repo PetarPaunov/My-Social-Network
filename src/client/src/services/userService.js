fb8fd95602ea7073969ca8abec68cb2a8f2f96ad
PetarPaunov <petarp.2000@abv.bg>
2023-03-11 14:24:37 +0200

    Added get all friends

--- src/client/src/services/userService.js
+++ src/client/src/services/userService.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const baseUrl = 'http://localhost:5236/api/UserProfile/';
 const getUserInfoUrl = baseUrl + 'user-profile';
-const updateUserInfo = baseUrl + 'update';
+const updateUserInfoUrl = baseUrl + 'update';
+const getAllFriendsUrl = baseUrl + 'friends';
 
 export const getUserInfo = async () => {
 
@@ -20,7 +21,7 @@ export const getUserInfo = async () => {
 };
 
 export const changeUserInfo = async (data) => {
-    const response = await axios.put(updateUserInfo, data, {
+    const response = await axios.put(updateUserInfoUrl, data, {
       headers: {
         "Content-Type": "multipart/form-data",
         Authorization:
@@ -30,4 +31,16 @@ export const changeUserInfo = async (data) => {
     });
   
     return response.data;
+  };
+
+  export const getAllFriends = async () => {
+    const response = await fetch(getAllFriendsUrl, {
+      headers: {
+        Authorization:
+          "Bearer " +
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6IjlhNzk5OGIxLTFmYmYtNGI3My04NDExLWJlMzE5Y2UxN2VlMyIsIm5hbWVpZCI6IjlhNzk5OGIxLTFmYmYtNGI3My04NDExLWJlMzE5Y2UxN2VlMyIsInN1YiI6IlRlc3RAVGVzdC5jb20iLCJlbWFpbCI6IlRlc3RAVGVzdC5jb20iLCJqdGkiOiJkNjBmNDZjMy0yZjFlLTQ4MzQtYTkxMi05MjNhYzgwY2FkM2YiLCJuYmYiOjE2Nzg1MzM2MjQsImV4cCI6MTY3ODYyMDAyMywiaWF0IjoxNjc4NTMzNjI0fQ.2AlIo-PFIp-k4WYdZn7FCb2RMSjuWJOmR0KKw8yJjus",
+      },
+    })
+
+    return response.json();
   };
