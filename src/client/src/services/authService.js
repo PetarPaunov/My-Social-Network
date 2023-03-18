295f70c844cf174b5d20fc04f55d490282d3f341
PetarPaunov <petarp.2000@abv.bg>
2023-03-18 18:39:46 +0200

    Added register service

--- src/client/src/services/authService.js
+++ src/client/src/services/authService.js
@@ -1,5 +1,6 @@
 const baseUrl = "http://localhost:5236/api/Account/";
 const loginUrl = baseUrl + "Login";
+const registerUrl = baseUrl + "register";
 
 export const login = async (credentials) => {
     const response = await fetch(loginUrl, {
@@ -15,16 +16,17 @@ export const login = async (credentials) => {
     return result;
 };
 
-// export const Login = async (credentials, JWT) => {
-//     const response = await fetch(loginUrl, {
-//         headers: {
-//             "Content-Type": "multipart/form-data",
-//              Authorization: "Bearer " + JWT
-//         },
-//         body: JSON.stringify(credentials)
-//     })
+export const register = async (credentials) => {
+    const response = await fetch(registerUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(credentials)
+    });
 
-//     const result = response.json();
+    const result = response.json();
 
-//     return result;
-// };
+    return result;
+}
+
