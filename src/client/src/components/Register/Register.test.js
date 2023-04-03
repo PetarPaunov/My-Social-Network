bfa88980dfa0168ef74339135be2efd612e409e7
PetarPaunov <petarp.2000@abv.bg>
2023-04-03 20:46:25 +0300

    Register component tested

---
+++ src/client/src/components/Register/Register.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+import { render, screen} from "@testing-library/react";
+
+import "@testing-library/jest-dom";
+
+import { Register } from "./Register";
+import { AuthContext } from "../../contexts/AuthContext";
+
+test("Register component should render corectly", () => {
+  const onSigning = () => {};
+
+  render(
+    <BrowserRouter>
+      <AuthContext.Provider value={{ onSigning }}>
+        <Register />
+      </AuthContext.Provider>
+    </BrowserRouter>
+  );
+
+  expect(screen.queryByText("Sing up")).toBeInTheDocument();
+});
